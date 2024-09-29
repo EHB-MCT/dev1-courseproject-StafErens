7f1f0399bba11e0a83cbb5f80330de6549db5e95
Staf Erens <Staf.erens@student.ehb.be>
2024-09-29 17:38:31 +0200

    Signature done

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,21 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+Signature();
+
+function Signature(){
+context.fillStyle = '##000000';
+context.fillRect(50, 50, 300, 300);
+
+context.fillStyle = '#9364D5';
+context.fillRect(175, 125, 50, 150);
+context.fillRect(125, 275, 50, 50);
+context.fillRect(225, 275, 50, 50);
+context.fillRect(125, 75, 50, 100);
+context.fillRect(225, 75, 50, 100);
+context.fillRect(75, 75, 50, 50);
+context.fillRect(275, 75, 50, 50);
+}
+//To EZ compared to 1-6
