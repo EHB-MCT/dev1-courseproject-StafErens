ba445a97a3e69f106f208d3f3154e3cae0920314
Staf Erens <Staf.erens@student.ehb.be>
2024-10-10 10:51:52 +0200

    SpaceInvader ctrl+s

--- spaceInvader.js
+++ spaceInvader.js
@@ -5,17 +5,17 @@ let context = canvas.getContext("2d");
 
 Signature();
 
-function Signature(){
-context.fillStyle = '##000000';
-context.fillRect(50, 50, 300, 300);
+function Signature() {
+    context.fillStyle = "#000000";
+    context.fillRect(50, 50, 300, 300);
 
-context.fillStyle = '#9364D5';
-context.fillRect(175, 125, 50, 150);
-context.fillRect(125, 275, 50, 50);
-context.fillRect(225, 275, 50, 50);
-context.fillRect(125, 75, 50, 100);
-context.fillRect(225, 75, 50, 100);
-context.fillRect(75, 75, 50, 50);
-context.fillRect(275, 75, 50, 50);
+    context.fillStyle = "#9364D5";
+    context.fillRect(175, 125, 50, 150);
+    context.fillRect(125, 275, 50, 50);
+    context.fillRect(225, 275, 50, 50);
+    context.fillRect(125, 75, 50, 100);
+    context.fillRect(225, 75, 50, 100);
+    context.fillRect(75, 75, 50, 50);
+    context.fillRect(275, 75, 50, 50);
 }
 //To EZ compared to 1-6
