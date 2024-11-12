0b51b5bb0b1e4f70e802ec8cad104423b453e6d5
Staf Erens <Staf.erens@student.ehb.be>
2024-11-12 09:43:59 +0100

    GenerativeDesign 2.2

---
+++ Projects/2-GenerativeDesign.js
@@ -0,0 +1,41 @@
+
+"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+import * as Utils from "../../scripts/utils.js";
+import * as Noise from "../../scripts/noise.js";
+
+
+
+Color();
+
+Grid();
+
+function Grid() {
+    context.strokeStyle = "black";
+    context.lineWidth = 5;
+    for (let i = 0; i < 30; i+= 1) {
+        context.beginPath();
+
+        for (let j = 0; j < window.outerHeight; j+= 50) {
+            let y = Noise.perlinNoise(j / 100) * 200 - 200;
+                context.lineTo(y + i * 100, j);
+                context.stroke();
+                //Instead of making a new lines I used rectangles as lines...
+                context.fillStyle = "black"
+                context.fillRect(y + i * 100, j, 200, 5)
+            
+        }
+    }
+}
+
+function Color() {
+    let RX = Utils.randomNumber(2, 10)
+
+        for (let i = 0; i < 20; i+= 1) {
+        context.fillStyle = Utils.hsl(200, 1  * RX * i , 50);
+        context.fillRect(0, 0 + 50 * i, window.outerWidth, 100)
+        }
+}
