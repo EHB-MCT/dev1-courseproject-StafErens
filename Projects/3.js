371c489baf169e5cbbe033c64945b112845adf7c
Staf Erens <Staf.erens@student.ehb.be>
2024-12-18 19:22:26 +0100

    Signature and cleanup 1.0

--- Projects/3.js
+++ Projects/3.js
@@ -7,17 +7,68 @@ let context = canvas.getContext("2d");
 
 import * as Utils from "../../scripts/utils.js";
 import * as Noise from "../../scripts/noise.js";
+//Noise scripts and other scripts from Solutions https://github.com/EHB-MCT/DEV1_Solutions_24.git
 
-Noisedraw();
+let play = true
+let Ysig= window.innerHeight;
+let Xsig = window.innerWidth;
 
-function Noisedraw() {
-    for (let i = 0; i < 30; i+= 1) {
+ColorBackground();
+Grid();
+
+Signature();
+
+
+function Grid() {
+    context.strokeStyle = "black";
+    context.lineWidth = 5;
+
+    for (let i = 0; i < window.outerWidth / 90; i+= 1) {
         context.beginPath();
-        for (let j = 0; j < 1000; j++) {
-            
+
+        for (let j = 0; j < window.outerHeight; j+= 50) {
+            //Used spacing and stroke line to get a more pointy look for the noise
             let y = Noise.perlinNoise(j / 100) * 200 - 200;
-                context.lineTo(y + i * 100, j, );
-                context.stroke(); 
+                context.lineTo(y + i * 100, j);
+                context.stroke();
+                //Instead of making a new lines I used rectangles as lines
+                context.fillStyle = "black";
+                context.fillRect(y + i * 100, j, 100, 5) 
         }
     }
+}
+
+function ColorBackground() {
+    let RX = Utils.randomNumber(2, 10)
+
+        for (let i = 0; i < 25; i+= 1) {
+            context.fillStyle = Utils.hsl(200, 1  + RX * i , 50);
+            context.fillRect(0, 0 + 50 * i, window.outerWidth, 100) 
+        }    
+}
+
+function Animation() {
+ if (play) {
+    ColorBackground()
+
+    requestAnimationFrame(Animation)
+
+ }
+}
+
+console.log(Ysig)
+console.log(Xsig)
+
+function Signature() {
+    context.fillStyle = "#000000";
+    context.fillRect(Xsig - 300, Ysig - 300, 300, 300);
+
+    context.fillStyle = "#9364D5";
+    context.fillRect(Xsig - 175,Ysig - 225, 50, 150);
+    context.fillRect(Xsig - 125, Ysig - 75, 50, 50);
+    context.fillRect(Xsig - 225, Ysig - 75, 50, 50);
+    context.fillRect(Xsig - 125, Ysig - 275, 50, 100);
+    context.fillRect(Xsig - 225, Ysig - 275, 50, 100);
+    context.fillRect(Xsig - 75, Ysig - 275, 50, 50);
+    context.fillRect(Xsig - 275, Ysig - 275, 50, 50);
 }
